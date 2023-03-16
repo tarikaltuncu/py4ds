5c22ae0ffd1e495aebb0e2a2cae9af5da4ebc5ae
Tarik Altuncu <tarikaltuncu@gmail.com>
2023-03-16 09:10:39 +0300

    week 3 materials

--- l2/c16_api_json.py
+++ l2/c16_api_json.py
@@ -6,14 +6,15 @@ response = requests.get("http://api.open-notify.org/iss-now.json")
 # Print the status code of the response.
 print(response.status_code)
 
-# Assign the JSON data to a variable.
-data = response.json()
-print(data)
+if response.status_code == 200:
+    # Assign the JSON data to a variable.
+    data = response.json()
+    print(data)
 
-# Print the latitude and longitude of the ISS.
-print(data['iss_position']['latitude'])
-print(data['iss_position']['longitude'])
+    # Print the latitude and longitude of the ISS.
+    print(data['iss_position']['latitude'])
+    print(data['iss_position']['longitude'])
 
-from c10_objects import Point
-iss_position = Point(float(data['iss_position']['latitude']), float(data['iss_position']['longitude']))
-print(iss_position)
+    from c10_objects import Point
+    iss_position = Point(float(data['iss_position']['latitude']), float(data['iss_position']['longitude']))
+    print(iss_position)
